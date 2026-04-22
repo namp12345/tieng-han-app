72525fbc3cf63df5411fca111d188c2909eb83a5
namp12345 <nguyenducthuan85@gmail.com>
2026-04-22 15:36:36 +0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,32 @@
-const CACHE_NAME = 'k-tour-guide-v6-cache-v1';
-const URLS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+// Service Worker for K-Tour Guide v6
+const CACHE_NAME = 'tieng-han-hdv-v6';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
 self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(URLS)));
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
+  );
 });
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
+    caches.keys().then(names => {
+      return Promise.all(
+        names.filter(name => name !== CACHE_NAME).map(name => caches.delete(name))
+      );
+    }).then(() => self.clients.claim())
   );
 });
 self.addEventListener('fetch', event => {
-  event.respondWith(caches.match(event.request).then(res => res || fetch(event.request)));
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+      .catch(() => caches.match('./index.html'))
+  );
 });
